6824bca335523986248af0f5a59384a70530ddb5
cloud <lehaoson@gmail.com>
2017-02-12 07:28:30 +0700

    Scaffolding Browser Route for child pages (About me, Universal UI..)

--- app.bundle.js
+++ app.bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://0.0.0.0:3000/";
+/******/ 	__webpack_require__.p = "https://localhost/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 251);
